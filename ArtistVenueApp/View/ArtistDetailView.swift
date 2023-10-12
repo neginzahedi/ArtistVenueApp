6ed3e13a5beb24b7e946e879441d7ab05f77e559
Negin Zahedi <zahedi.negin@gmail.com>
2023-10-12 18:01:50 -0400

    refactor: Finalized UI and create subviews to improve code clarity

--- ArtistVenueApp/View/ArtistDetailView.swift
+++ ArtistVenueApp/View/ArtistDetailView.swift
@@ -12,52 +12,78 @@ struct ArtistDetailView: View {
     var artist: Artist
     
     @StateObject var api = DataManager()
-    
-    @State var performances = [ArtistPerformance]()
-    @State private var artistImage: UIImage?
+    @State private var performances = [ArtistPerformance]()
     @State private var venueImages = [String:UIImage]()
     
     var body: some View {
-        VStack{
-            Image(uiImage: artistImage ?? UIImage())
-                .resizable()
-                .frame(width: 200, height: 200)
-            Text(artist.name)
-            List(performances){ performance in
-                HStack{
-                    Image(uiImage: venueImages[performance.venue.name] ?? UIImage())
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 200, height: 200)
-                    Text(performance.venue.name)
-                    Text(performance.date)
-                }
+        PerformancesListView(performances: performances, venueImages : venueImages)
+            .onAppear(){
+                fetchArtistDetail()
             }
-        }
-        
-        .onAppear(){
-            api.fetchArtistPerformances(artistID: artist.id) { performances in
-                self.performances = performances
-                
-                for performance in performances {
-                    
-                    api.fetchVenueImage(venueName: performance.venue.name) { image in
-                        if let image = image {
-                            self.venueImages[performance.venue.name] = image
-                        }
+            .fontDesign(.rounded)
+            .navigationTitle(artist.name)
+    }
+    
+    private func fetchArtistDetail() {
+        api.fetchArtistPerformances(artistID: artist.id) { performances in
+            self.performances = performances
+            
+            for performance in performances {
+                api.fetchVenueImage(venueName: performance.venue.name) { image in
+                    if let image = image {
+                        self.venueImages[performance.venue.name] = image
                     }
                 }
-            }
-            
-            api.fetchArtistImage(artistName: artist.name) { image in
-                self.artistImage = image
             }
         }
     }
 }
 
-/*
- #Preview {
- ArtistDetailView()
- }
- */
+struct PerformancesListView: View {
+    let performances: [ArtistPerformance]
+    let venueImages: [String:UIImage]
+    
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false){
+            VStack(alignment: .leading){
+                HStack{
+                    Text("Performances")
+                        .font(.title2)
+                        .bold()
+                    Spacer()
+                }
+                ForEach(performances) { performance in
+                    VenueCard(performance: performance, venueImage: venueImages[performance.venue.name] ?? UIImage())
+                    Divider()
+                }
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            
+        }
+    }
+}
+
+struct VenueCard: View {
+    let performance: ArtistPerformance
+    let venueImage: UIImage
+    
+    var body: some View {
+        HStack{
+            Image(uiImage: venueImage)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 80, height: 80)
+            VStack(alignment: .leading) {
+                HStack{
+                    Text(DataManager.formatDateString(performance.date))
+                        .bold()
+                    Text(DataManager.formatTimeString(performance.date))
+                        .foregroundStyle(.secondary)
+                }
+                Text(performance.venue.name)
+                    .font(.caption)
+            }
+        }
+    }
+}

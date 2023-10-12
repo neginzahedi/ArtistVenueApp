da69237603d4eb1b9fa600e529d54de01d23fffa
Negin Zahedi <zahedi.negin@gmail.com>
2023-10-12 10:13:17 -0400

    Initial Commit

---
+++ ArtistVenueApp/ArtistVenueAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtistVenueAppApp.swift
+//  ArtistVenueApp
+//
+//  Created by Negin Zahedi on 2023-10-12.
+//
+
+import SwiftUI
+
+@main
+struct ArtistVenueAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArtistVenueApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ArtistVenueApp
+//
+//  Created by Negin Zahedi on 2023-10-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

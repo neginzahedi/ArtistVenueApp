7698b1f9bba5469f2f45871b23267c6f73cea468
Negin Zahedi <zahedi.negin@gmail.com>
2023-10-12 21:04:11 -0400

    test: Add unit tests for async and helper methods in DataManager ViewModel

--- ArtistVenueAppTests/ArtistVenueAppTests.swift
+++ ArtistVenueAppTests/ArtistVenueAppTests.swift
@@ -6,30 +6,86 @@
 //
 
 import XCTest
+@testable import ArtistVenueApp
 
 final class ArtistVenueAppTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // MARK: - Testing Async Methods
+    
+    func testFetchArtists() async {
+        // Given
+        let dataManager = DataManager()
+        // When
+        let result = try? await dataManager.fetchArtists()
+        // Then
+        XCTAssertNotNil(result)
+        XCTAssert(result!.capacity > 0)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testFetchArtistPerformances() async{
+        // Given
+        let dataManager = DataManager()
+        // When
+        let result = try? await dataManager.fetchArtistPerformances(artistID: 4)
+        // Then
+        XCTAssertNotNil(result)
+        XCTAssert(result!.capacity >= 0)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testFetchImage() async{
+        // Given
+        let dataManager = DataManager()
+        let invalidURL = URL(string: "\(dataManager.BASE_IMG_URL)/v/The+Dive.png")
+        let validURL = URL(string: "\(dataManager.BASE_IMG_URL)/venues/The+Dive.png")
+        // When
+        let resultInvalid = try? await dataManager.fetchImage(url: invalidURL!)
+        let resultValid = try? await dataManager.fetchImage(url: validURL!)
+        // Then
+        XCTAssertNil(resultInvalid)
+        XCTAssertNotNil(resultValid)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    // MARK: - Testing Helper Methods
+    
+    func testTwoWeekRange() {
+        // Given
+        let customDate = "2023-10-01"
+        let expectedOutput = "from=2023-10-01&to=2023-10-14"
+        // When
+        // Converting the 'customDate' to Date object
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let date = dateFormatter.date(from: customDate)!
+        // Getting the result of the twoWeekRange function
+        let result = DataManager.twoWeekRange(from: date)
+        // Then
+        XCTAssertEqual(result, expectedOutput, "Date conversion result does not match expected date")
     }
-
+    
+    func testFormatTimeString(){
+        // Given
+        let expectedOutput = "6:00 PM"
+        // When
+        let result = DataManager.formatTimeString("2026-08-02T18:00:00")
+        // Then
+        XCTAssertEqual(result, expectedOutput, "Time hr is not formatted properly")
+    }
+    
+    func testFormatDateString(){
+        // Given
+        let expectedOutput = "Aug 2, 2026"
+        // When
+        let result = DataManager.formatDateString("2026-08-02T18:00:00")
+        // Then
+        XCTAssertEqual(result, expectedOutput, "Date is not formatted properly")
+    }
+    
+    func testGetImageURL(){
+        // Given
+        let dataManager = DataManager()
+        // When
+        let result = try? dataManager.getImageURL(name: "The Dive", type: "venues")
+        // Then
+        XCTAssertEqual(result, URL(string:"\(dataManager.BASE_IMG_URL)/venues/The+Dive.png"))
+    }
 }
